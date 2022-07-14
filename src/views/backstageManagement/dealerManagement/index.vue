60cddd75a2e497f5d4b8300a8d59ab29e7dc3c73
JoeTzeng <joe.tzeng@9talent.com>
2022-07-14 15:44:21 +0800

    上傳

--- src/views/backstageManagement/dealerManagement/index.vue
+++ src/views/backstageManagement/dealerManagement/index.vue
@@ -1,97 +1,99 @@
 <template>
-  <div class="view-container">
-    <div class="view-container-seachForm">
-      <div class="view-container-seachForm-item seachForm-expand">
-        <p class="view-container-seachForm-item-wrap pc">
-          <el-button class="bg-yellow" size="mini" @click="onSearchBtnClick(searchForm, currentPage)">{{ $t("__refresh") }}</el-button>
-        </p>
-        <p class="view-container-seachForm-item-wrap">
-          <el-input v-model="searchForm.id" type="number" placeholder="ID" />
-        </p>
-        <p class="view-container-seachForm-item-wrap">
-          <el-input v-model="searchForm.account" :placeholder="$t('__account')" />
-        </p>
-        <p class="view-container-seachForm-item-wrap">
-          <el-input v-model="searchForm.name" :placeholder="$t('__name')" />
-        </p>
-        <p class="view-container-seachForm-item-wrap">
-          <el-select v-model="searchForm.status" multiple filterable :collapse-tags="statusCollapse" :placeholder="$t('__status')">
-            <el-option v-for="item in searchItems.status" :key="item.key" :label="item.nickname" :value="item.key" />
-          </el-select>
-        </p>
-      </div>
-      <div class="view-container-seachForm-item">
-        <div class="view-container-seachForm-item-group">
+  <div>
+    <div ref="container" class="view-container">
+      <div ref="seachForm" class="view-container-seachForm" :class="{'opened': sidebar.opened}">
+        <div ref="seachFormExpand" class="view-container-seachForm-item">
           <p class="view-container-seachForm-item-wrap pc">
-            <el-button class="bg-gray" size="mini" @click="onSearchBtnClick({}, 1)">{{ $t("__reset") }}</el-button>
+            <el-button class="bg-yellow" size="mini" @click="onSearchBtnClick(searchForm, currentPage)">{{ $t("__refresh") }}</el-button>
           </p>
           <p class="view-container-seachForm-item-wrap">
-            <el-button class="bg-yellow" size="mini" @click="onSearchBtnClick(searchForm, 1)">
-              {{ $t("__search") }}
-            </el-button>
+            <el-input v-model="searchForm.id" type="number" placeholder="ID" />
           </p>
           <p class="view-container-seachForm-item-wrap">
-            <el-button class="bg-yellow" size="mini" @click="onCreateBtnClick()">
-              {{ $t("__create") }}
-            </el-button>
+            <el-input v-model="searchForm.account" :placeholder="$t('__account')" />
           </p>
           <p class="view-container-seachForm-item-wrap">
-            <el-button class="moreSearch" size="mini" :icon="advancedSearchIcon" @click="searchFormOpen = !searchFormOpen">
-              {{ $t("__moreSearch") }}
-            </el-button>
+            <el-input v-model="searchForm.name" :placeholder="$t('__name')" />
           </p>
+          <p class="view-container-seachForm-item-wrap">
+            <el-select v-model="searchForm.status" multiple filterable :collapse-tags="statusCollapse" :placeholder="$t('__status')">
+              <el-option v-for="item in searchItems.status" :key="item.key" :label="item.nickname" :value="item.key" />
+            </el-select>
+          </p>
+        </div>
+        <div class="view-container-seachForm-item">
+          <div class="view-container-seachForm-item-group">
+            <p class="view-container-seachForm-item-wrap pc">
+              <el-button class="bg-gray" size="mini" @click="onSearchBtnClick({}, 1)">{{ $t("__reset") }}</el-button>
+            </p>
+            <p class="view-container-seachForm-item-wrap">
+              <el-button class="bg-yellow" size="mini" @click="onSearchBtnClick(searchForm, 1)">
+                {{ $t("__search") }}
+              </el-button>
+            </p>
+            <p class="view-container-seachForm-item-wrap">
+              <el-button class="bg-yellow" size="mini" @click="onCreateBtnClick()">
+                {{ $t("__create") }}
+              </el-button>
+            </p>
+            <p class="view-container-seachForm-item-wrap">
+              <el-button class="moreSearch" size="mini" :icon="advancedSearchIcon" @click="searchFormOpen = !searchFormOpen">
+                {{ $t("__moreSearch") }}
+              </el-button>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div ref="table" class="view-container-table">
+        <div
+          v-for="(item, index) in tableData"
+          :key="index"
+          class="view-container-table-row"
+          :class="{'single-row': index % 2 === 0}"
+        >
+          <img v-if="item.photo_url === ''" class="dealerPhoto" src="@/assets/unknown.png" :alt="$t('__dealerPhoto')">
+          <img v-else :src="item.photo_url" class="dealerPhoto" :alt="$t('__dealerPhoto')">
+          <table>
+            <tr>
+              <td>
+                <span class="header">ID:</span>
+                <span>{{ item.id }}</span>
+              </td>
+              <td>
+                <span class="header">{{ $t('__account') }}:</span>
+                <span>{{ item.account }}</span>
+              </td>
+              <td>
+                <span class="header">{{ $t('__name') }}:</span>
+                <span>{{ item.name }}</span>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <span class="header">{{ $t('__activated') }}:</span>
+                <span class="status" :class="{'statusOpen': item.status === '1' }">{{ item.statusLabel }}</span>
+              </td>
+              <td>
+                <span class="header">{{ $t('__creator') }}:</span>
+                <span>{{ item.creator }}</span>
+              </td>
+              <td>
+                <div class="operate">
+                  <el-button class="bg-yellow" size="mini" @click="onEditBtnClick(scope.row)">{{ $t("__loginBarcode") }}</el-button>
+                  <a :href="item.dns1d" :download="item.name">
+                    <el-button class="bg-yellow" size="mini">{{ $t("__loginBarcodeDownload") }}</el-button>
+                  </a>
+                  <el-button class="bg-yellow" size="mini" @click="onEditBtnClick(scope.row)">{{ $t("__edit") }}</el-button>
+                </div>
+              </td>
+            </tr>
+          </table>
         </div>
       </div>
     </div>
-    <div class="view-container-table">
-      <div
-        v-for="(item, index) in tableData"
-        :key="index"
-        class="view-container-table-row"
-        :class="{'single-row': index % 2 === 0}"
-      >
-        <img v-if="item.photo_url === ''" class="dealerPhoto" src="@/assets/unknown.png" :alt="$t('__dealerPhoto')">
-        <img v-else :src="item.photo_url" class="dealerPhoto" :alt="$t('__dealerPhoto')">
-        <table>
-          <tr>
-            <td>
-              <span class="header">ID:</span>
-              <span>{{ item.id }}</span>
-            </td>
-            <td>
-              <span class="header">{{ $t('__account') }}:</span>
-              <span>{{ item.account }}</span>
-            </td>
-            <td>
-              <span class="header">{{ $t('__name') }}:</span>
-              <span>{{ item.name }}</span>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <span class="header">{{ $t('__activated') }}:</span>
-              <span class="status" :class="{'statusOpen': item.status === '1' }">{{ item.statusLabel }}</span>
-            </td>
-            <td>
-              <span class="header">{{ $t('__creator') }}:</span>
-              <span>{{ item.creator }}</span>
-            </td>
-            <td>
-              <div class="operate">
-                <el-button class="bg-yellow" size="mini" @click="onEditBtnClick(scope.row)">{{ $t("__loginBarcode") }}</el-button>
-                <a :href="item.dns1d" :download="item.name">
-                  <el-button class="bg-yellow" size="mini">{{ $t("__loginBarcodeDownload") }}</el-button>
-                </a>
-                <el-button class="bg-yellow" size="mini" @click="onEditBtnClick(scope.row)">{{ $t("__edit") }}</el-button>
-              </div>
-            </td>
-          </tr>
-        </table>
-      </div>
-    </div>
-    <div class="view-container-footer" :class="{'opened': sidebar.opened}">
+    <div class="view-footer" :class="{'opened': sidebar.opened}">
       <el-pagination
-        :layout="pagerLayout"
+        layout="prev, pager, next, jumper, sizes"
         :total="totalCount"
         background
         :page-size="pageSize"
@@ -124,8 +126,7 @@ export default {
         'edit': 2
       }),
       curDialogIndex: 0,
-      paginationPagerCount: 3,
-      paginationLayout: "prev, pager, next, sizes"
+      paginationPagerCount: 5
     }
   },
   computed: {
@@ -136,10 +137,7 @@ export default {
       return this.searchForm.status && this.searchForm.status.length > this.selectCollapseCount;
     },
     pagerCount() {
-      return this.paginationPagerCount;
-    },
-    pagerLayout() {
-      return this.paginationLayout;
+      return this.paginationPagerCount
     }
   },
   watch: {
@@ -150,25 +148,20 @@ export default {
   methods: {
     resizeHandler() {
       const vw = window.innerWidth;
-      const seachFormItem = document.getElementsByClassName("seachForm-expand");
-      var formHeight = "34px";
+      var formHeight = 34;
       if (vw <= 768) {
-        formHeight = this.searchFormOpen ? `${136 + (this.searchForm.status.length * 32)}px` : formHeight;
-        this.paginationPagerCount = 3;
-        this.paginationLayout = "prev, pager, next, sizes";
+        formHeight = this.searchFormOpen ? 136 + (this.searchForm.status.length * 32) : formHeight;
+        this.paginationPagerCount = 5;
       } else if (vw > 768 && vw <= 992) {
-        formHeight = this.searchFormOpen ? `${68 + (this.searchForm.status.length * 32)}px` : formHeight;
+        formHeight = this.searchFormOpen ? 68 + (this.searchForm.status.length * 32) : formHeight;
         this.paginationPagerCount = 7;
-        this.paginationLayout = "prev, pager, next, jumper, sizes";
       } else {
-        formHeight = `${34 + (this.searchForm.status.length * 32)}px`;
+        formHeight = 34 + (this.searchForm.status.length * 32);
         this.paginationPagerCount = 7;
-        this.paginationLayout = "prev, pager, next, jumper, sizes";
       }
 
-      seachFormItem.forEach(item => {
-        item.style.height = formHeight;
-      });
+      this.$refs.seachFormExpand.style.height = `${formHeight}px`;
+      this.$refs.table.style.top = `${this.$refs.seachForm.clientHeight}px`;
     },
     onSearchBtnClick(data, page) {
       this.searchForm = data
@@ -183,6 +176,8 @@ export default {
       })
     },
     handleRespone(res) {
+      this.$refs.container.scrollTop = 0;
+      this.$refs.table.scrollTop = 0;
       this.searchItems = res.searchItems
       this.totalCount = res.rows.length
       this.allDataByClient = res.rows
@@ -226,30 +221,32 @@ export default {
           vertical-align: middle;
           width: 105px;
         }
-        tr {
-          display: flex;
-          flex-direction: column;
-          td {
-            line-height: 20px;
-            .header {
-              font-weight: bold;
-              margin-right: 5px;
-            }
-            .status {
-              color: #f00;
-              font-weight: bold;
-            }
-            .statusOpen {
-              color: #090;
-            }
-            .operate {
-              width: 250px;
-              display: flex;
-              justify-content: space-between;
-              a {
-                .el-button {
-                  height: 100%;
-                  vertical-align: top;
+        table {
+          tr {
+            display: flex;
+            flex-direction: column;
+            td {
+              line-height: 20px;
+              .header {
+                font-weight: bold;
+                margin-right: 5px;
+              }
+              .status {
+                color: #f00;
+                font-weight: bold;
+              }
+              .statusOpen {
+                color: #090;
+              }
+              .operate {
+                width: 250px;
+                display: flex;
+                justify-content: space-between;
+                a {
+                  .el-button {
+                    height: 100%;
+                    vertical-align: top;
+                  }
                 }
               }
             }
@@ -299,14 +296,12 @@ export default {
                 width: 350px;
                 font-size: 20px;
                 line-height: 60px;
-
                 .operate {
                   width: 100%;
                   .el-button {
                     font-size: 20px;
                   }
                 }
-
               }
             }
           }
