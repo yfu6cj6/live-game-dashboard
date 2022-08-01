ecd1be72dd5e2d5341858460f0d89c3b7f6b529a
JoeTzeng <joe.tzeng@9talent.com>
2022-08-01 11:58:54 +0800

    上傳

--- src/components/gameResult/gameResultDialog.vue
+++ src/components/gameResult/gameResultDialog.vue
@@ -228,6 +228,95 @@
         </tr>
       </table>
     </div>
+    <div class="smallEyeRoadData">
+      <table>
+        <tr
+          v-for="(road, i) in smallEyeRoad"
+          :key="i"
+        >
+          <td
+            v-for="(item, j) in road"
+            :key="j"
+            class="item"
+          >
+            <div
+              :class="{
+                'smallEyeRoad-current': item.split('_')[1] === '1'
+              }"
+            />
+            <div
+              :class="{
+                'smallEyeRoad-winner-banker': item.split('_')[0] === '1',
+                'smallEyeRoad-winner-player': item.split('_')[0] === '2'
+              }"
+            />
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div class="cockroachRoadData">
+      <table>
+        <tr
+          v-for="(road, i) in cockroachRoad"
+          :key="i"
+        >
+          <td
+            v-for="(item, j) in road"
+            :key="j"
+            class="item"
+          >
+            <div
+              :class="{
+                'cockroachRoad-current': item.split('_')[1] === '1'
+              }"
+            />
+            <div
+              :class="{
+                'cockroachRoad-winner-banker': item.split('_')[0] === '1',
+                'cockroachRoad-winner-player': item.split('_')[0] === '2'
+              }"
+            />
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div class="beadRoadData">
+      <table>
+        <tr
+          v-for="(road, i) in beadRoad"
+          :key="i"
+        >
+          <td
+            v-for="(item, j) in road"
+            :key="j"
+            class="item"
+          >
+            <div
+              :class="{
+                'beadRoad-current': item.split('_')[2] === '1'
+              }"
+            />
+            <div
+              :class="{
+                'beadRoad-winner-banker': item.split('_')[0] === '1',
+                'beadRoad-winner-player': item.split('_')[0] === '2',
+                'beadRoad-winner-tie': item.split('_')[0] === '3'
+              }"
+            />
+            <div
+              :class="{
+                'beadRoad-pair-banker': item.split('_')[1] === '1' || item.split('_')[1] === '3',
+              }"
+            />
+            <div
+              :class="{
+                'beadRoad-pair-player': item.split('_')[1] === '2' || item.split('_')[1] === '3',
+              }"
+            />
+          </td>
+        </tr>
+      </table>
+    </div>
   </el-dialog>
 </template>
 
@@ -325,6 +414,58 @@ export default {
           bigEyeRoad[i] = bigEyeRoad[i].slice(0, bigEyeRoadLastIndex);
         }
         this.bigEyeRoad = bigEyeRoad;
+
+        const smallEyeRoad = getRoadArray(3, roadData, this.roundInfo.scoreCardsId)
+        var smallEyeRoadLastIndex = 0
+        smallEyeRoad.forEach(element => {
+          const info = element.filter(item => { return item !== "" })
+          const last = element.lastIndexOf(info[info.length - 1])
+          if (smallEyeRoadLastIndex < last) {
+            smallEyeRoadLastIndex = last
+          }
+        })
+        if (smallEyeRoadLastIndex < 58) {
+          smallEyeRoadLastIndex = 58
+        }
+        for (let i = 0, max = smallEyeRoad.length; i < max; i++) {
+          smallEyeRoad[i] = smallEyeRoad[i].slice(0, smallEyeRoadLastIndex);
+        }
+        this.smallEyeRoad = smallEyeRoad;
+
+        const cockroachRoad = getRoadArray(4, roadData, this.roundInfo.scoreCardsId)
+        var cockroachRoadLastIndex = 0
+        cockroachRoad.forEach(element => {
+          const info = element.filter(item => { return item !== "" })
+          const last = element.lastIndexOf(info[info.length - 1])
+          if (cockroachRoadLastIndex < last) {
+            cockroachRoadLastIndex = last
+          }
+        })
+        if (cockroachRoadLastIndex < 58) {
+          cockroachRoadLastIndex = 58
+        }
+        for (let i = 0, max = cockroachRoad.length; i < max; i++) {
+          cockroachRoad[i] = cockroachRoad[i].slice(0, cockroachRoadLastIndex);
+        }
+        this.cockroachRoad = cockroachRoad;
+
+        const beadRoad = getRoadArray(1, roadData, this.roundInfo.scoreCardsId)
+        var beadRoadLastIndex = 0
+        beadRoad.forEach(element => {
+          const info = element.filter(item => { return item !== "" })
+          const last = element.lastIndexOf(info[info.length - 1])
+          if (beadRoadLastIndex < last) {
+            beadRoadLastIndex = last
+          }
+        })
+        console.log(beadRoadLastIndex)
+        if (beadRoadLastIndex < 29) {
+          beadRoadLastIndex = 29
+        }
+        for (let i = 0, max = beadRoad.length; i < max; i++) {
+          beadRoad[i] = beadRoad[i].slice(0, beadRoadLastIndex);
+        }
+        this.beadRoad = beadRoad;
 
         this.dialogLoading = false
       } else {
@@ -432,7 +573,8 @@ export default {
 
 .bigRoadData {
   margin-top: 10px;
-  overflow: auto;
+  overflow-x: auto;
+  overflow-y: hidden;
   table {
     background-color: #fff;
     border-spacing: 0;
@@ -522,7 +664,8 @@ export default {
   }
 }
 .bigEyeRoadData {
-  overflow: auto;
+  overflow-x: auto;
+  overflow-y: hidden;
   table {
     background-color: #fff;
     border-spacing: 0;
@@ -576,6 +719,220 @@ export default {
             &-player {
               border: 2px solid $blue;
             }
+          }
+        }
+      }
+    }
+  }
+}
+.smallEyeRoadData {
+  overflow-x: auto;
+  overflow-y: hidden;
+  table {
+    background-color: #fff;
+    border-spacing: 0;
+    border-collapse: collapse;
+    &:first-child {
+      border-top: 1px solid #aaa;
+    }
+    tr {
+      border-left: 1px solid #aaa;
+      border-right: 1px solid #aaa;
+      &:nth-child(even) {
+        border-bottom: 1px solid #aaa;
+      }
+      td {
+        &:nth-child(even) {
+          border-right: 1px solid #aaa;
+        }
+        width: 10px;
+        min-width: 10px;
+        height: 10px;
+        min-height: 10px;
+        max-height: 10px;
+        position: relative;
+        .smallEyeRoad {
+          &-current {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            animation: change 1s;
+            animation-iteration-count: infinite;
+            animation-direction: alternate;
+          }
+          &-winner {
+            &-banker,
+            &-player {
+              position: absolute;
+              top: 50%;
+              left: 50%;
+              transform: translateX(-50%) translateY(-50%);
+              width: calc(100% - 1px);
+              height: calc(100% - 1px);
+              border-radius: 50%;
+            }
+          }
+          &-winner {
+            &-banker {
+              background-color: $red;
+            }
+            &-player {
+              background-color: $blue;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+.cockroachRoadData {
+  overflow-x: auto;
+  overflow-y: hidden;
+  table {
+    background-color: #fff;
+    border-spacing: 0;
+    border-collapse: collapse;
+    &:first-child {
+      border-top: 1px solid #aaa;
+    }
+    tr {
+      border-left: 1px solid #aaa;
+      border-right: 1px solid #aaa;
+      &:nth-child(even) {
+        border-bottom: 1px solid #aaa;
+      }
+      td {
+        &:nth-child(even) {
+          border-right: 1px solid #aaa;
+        }
+        width: 10px;
+        min-width: 10px;
+        height: 10px;
+        min-height: 10px;
+        max-height: 10px;
+        position: relative;
+        .cockroachRoad {
+          &-current {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            animation: change 1s;
+            animation-iteration-count: infinite;
+            animation-direction: alternate;
+          }
+          &-winner {
+            &-banker,
+            &-player {
+              position: absolute;
+              top: 50%;
+              left: 50%;
+              transform: translateX(-25%) translateY(-25%) rotate(45deg);
+              width: calc(100% - 1px);
+              height: calc(100% - 1px);
+            }
+          }
+          &-winner {
+            &-banker {
+              border-left: 2px solid $red;
+            }
+            &-player {
+              border-left: 2px solid $blue;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+.beadRoadData {
+  overflow-x: auto;
+  overflow-y: hidden;
+  table {
+    background-color: #fff;
+    border-spacing: 0;
+    border-collapse: collapse;
+    td {
+      width: 20px;
+      min-width: 20px;
+      height: 20px;
+      min-height: 20px;
+      max-height: 20px;
+      border: 1px solid #aaa;
+      position: relative;
+      .beadRoad {
+        &-current {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          animation: change 1s;
+          animation-iteration-count: infinite;
+          animation-direction: alternate;
+        }
+        &-winner {
+          &-banker,
+          &-player,
+          &-tie {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translateX(-50%) translateY(-50%);
+            width: 15px;
+            height: 15px;
+            border-radius: 50%;
+            &::after {
+              position: absolute;
+              color: #fff;
+              top: 50%;
+              left: 50%;
+              transform: translateX(-50%) translateY(-50%) scale(0.8);
+            }
+          }
+        }
+        &-winner {
+          &-banker {
+            background-color: $red;
+            &::after {
+              content: "莊";
+            }
+          }
+          &-player {
+            background-color: $blue;
+            &::after {
+              content: "閒";
+            }
+          }
+          &-tie {
+            background-color: $green;
+            &::after {
+              content: "和";
+            }
+          }
+        }
+        &-pair {
+          &-banker,
+          &-player {
+            position: absolute;
+            width: 6px;
+            height: 6px;
+            border-radius: 50%;
+          }
+        }
+        &-pair {
+          &-banker {
+            background-color: $darkRed;
+            top: 1px;
+            left: 1px;
+          }
+          &-player {
+            background-color: $darkBlue;
+            bottom: 1px;
+            right: 1px;
           }
         }
       }
