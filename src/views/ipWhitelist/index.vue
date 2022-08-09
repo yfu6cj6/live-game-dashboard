3216e953e8f12ec73aa7749b2b040346e1450699
DD <dd.chang@9talent.com>
2022-08-09 14:45:31 +0800

    上傳 ip白名單 新增樣式

--- src/views/ipWhitelist/index.vue
+++ src/views/ipWhitelist/index.vue
@@ -1,74 +1,49 @@
 <template>
-  <div v-loading="dataLoading">
-    <div ref="container" class="view-container">
-      <div ref="seachForm" class="view-container-seachForm">
-        <template v-if="device === 'mobile'">
-          <div ref="seachFormExpand" class="view-container-seachForm-option">
-            <p class="optionItem">
-              <el-input v-model="searchForm.account" :placeholder="$t('__account')" />
-            </p>
-            <p class="optionItem">
-              <el-input v-model="searchForm.ip" placeholder="IP" />
-            </p>
+  <div v-loading="dataLoading" class="tableStyle">
+    <template v-if="device === 'mobile'">
+      <div class="bg-black">
+        <div class="d-flex searchFrame">
+          <div class="d-flex pad_frame wrap">
+            <div class="pad_frame">
+              <div class="d-flex searchItemBox">
+                <div class="searchItem pad_frame">
+                  <div class="input_filter">
+                    <el-input v-model="searchForm.account" class="input_size" :placeholder="$t('__agentOrSubAccount')" />
+                  </div>
+                </div>
+                <div class="searchItem pad_frame">
+                  <div class="input_filter">
+                    <el-input v-model="searchForm.ip" class="input_size" placeholder="IP" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex ctrlBtn searchBtn">
+              <svg-icon class="searchIcon" icon-class="search" @click.stop="onSearchBtnClick(searchForm, 1)" />
+            </div>
+            <div v-if="isAdminister" class="d-flex pad_frame">
+              <el-button class="bg-yellow addBtn" size="mini" @click="onCreateBtnClick()">{{ $t("__create") }}</el-button>
+            </div>
           </div>
-          <div class="view-container-seachForm-operate">
-            <p class="operateItem">
-              <el-button class="bg-gray" size="mini" @click="onSearchBtnClick(searchForm, 1)">
-                {{ $t("__search") }}
-              </el-button>
-            </p>
-            <p class="operateItem">
-              <el-button class="bg-yellow" size="mini" @click="onCreateBtnClick()">
-                {{ $t("__create") }}
-              </el-button>
-            </p>
-            <p class="operateItem moreSearch">
-              <el-button class="bg-parent" size="mini" :icon="advancedSearchIcon" @click="searchFormOpen = !searchFormOpen">
-                {{ $t("__moreSearch") }}
-              </el-button>
-            </p>
-          </div>
-        </template>
-        <template v-else>
-          <div ref="seachFormExpand" class="view-container-seachForm-option">
-            <p class="optionItem">
-              <el-button class="bg-yellow" size="mini" @click="onSearchBtnClick(searchForm, currentPage)">{{ $t("__refresh") }}</el-button>
-            </p>
-            <p class="optionItem">
-              <el-input v-model="searchForm.account" :placeholder="$t('__account')" />
-            </p>
-            <p class="optionItem">
-              <el-input v-model="searchForm.ip" placeholder="IP" />
-            </p>
-            <p class="optionItem">
-              <el-button class="bg-gray" size="mini" @click="onSearchBtnClick({}, 1)">{{ $t("__reset") }}</el-button>
-            </p>
-            <p class="optionItem">
-              <el-button class="bg-yellow" size="mini" @click="onSearchBtnClick(searchForm, 1)">
-                {{ $t("__search") }}
-              </el-button>
-            </p>
-            <p class="optionItem">
-              <el-button class="bg-yellow" size="mini" @click="onCreateBtnClick()">
-                {{ $t("__create") }}
-              </el-button>
-            </p>
-          </div>
-        </template>
+        </div>
       </div>
-      <div ref="table" class="view-container-table">
-        <div v-if="tableData.length > 0">
-          <div
-            v-for="(item, index) in tableData"
-            :key="index"
-            class="view-container-table-row"
-            :class="{'single-row': index % 2 === 0}"
-          >
-            <template v-if="device === 'mobile'">
-              <div class="base">
+    </template>
+    <template v-else>
+      -
+    </template>
+    <div>
+      <div v-if="allDataByClient.length > 0">
+        <div
+          v-for="(item, index) in allDataByClient"
+          :key="index"
+          :class="{'odd-row': index % 2 === 0, 'even-row': index % 2 !== 0}"
+        >
+          <template v-if="device === 'mobile'">
+            <div class="dataContent">
+              <div class="d-flex base">
                 <div class="left">
                   <div class="field">
-                    <span class="title">{{ $t('__account') }}</span>
+                    <span class="title">{{ $t('__agentOrSubAccount') }}</span>
                     <div class="accountnews">
                       <svg-icon icon-class="user" class="icon" />
                       <span class="news spcolor">{{ item.account }}</span>
@@ -80,63 +55,41 @@
                     <span class="title">IP</span>
                     <span class="news">{{ item.ip }}</span>
                   </div>
-                  <div v-if="isAdminister" class="setup">
-                    <el-button class="bg-yellow" size="mini" @click="onEditBtnClick(item)">{{ $t("__edit") }}</el-button>
-                    <el-button class="bg-red" size="mini" @click="onDeleteBtnClick(item)">{{ $t("__delete") }}</el-button>
-                  </div>
                 </div>
                 <div :class="{'moreopen': !item.open, 'moreclose': item.open}" @click.stop="remarkExpand(item)">
-                  <svg-icon v-if="item.open" icon-class="up" />
-                  <svg-icon v-else icon-class="more" />
+                  <svg-icon v-if="item.open" class="icon" icon-class="up" />
+                  <svg-icon v-else class="icon" icon-class="more" />
                 </div>
               </div>
-              <div v-if="item.open">
-                <div class="field">
-                  <span class="title">{{ $t('__updateDate') }}</span>
-                  <span class="news">{{ item.updated_at }}</span>
+              <div v-if="item.open" class="expandInfo">
+                <div class="left">
+                  <div class="field">
+                    <span class="title">{{ $t('__updateDate') }}</span>
+                    <span class="news">{{ item.updated_at }}</span>
+                  </div>
+                </div>
+                <div v-if="isAdminister" class="right">
+                  <div class="adminCtrl">
+                    <el-button class="bg-red" size="mini" @click="onDeleteBtnClick(item)">{{ $t("__delete") }}</el-button>
+                  </div>
+                  <div class="adminCtrl">
+                    <el-button class="bg-yellow" size="mini" @click="onEditBtnClick(item)">{{ $t("__edit") }}</el-button>
+                  </div>
                 </div>
               </div>
-            </template>
-            <template v-else>
-              <!-- <div class="item id">
-                <span class="header">ID</span>
-                <span>{{ item.id }}</span>
-              </div>
-              <div class="item">
-                <span class="header">{{ $t('__account') }}</span>
-                <span>{{ item.account }}</span>
-              </div>
-              <div class="item">
-                <span class="header">IP</span>
-                <span>{{ item.ip }}</span>
-              </div>
-              <div class="operate">
-                <el-button class="bg-yellow" size="mini" @click="onEditBtnClick(item)">{{ $t("__edit") }}</el-button>
-                <el-button class="bg-red" size="mini" @click="onDeleteBtnClick(item)">{{ $t("__delete") }}</el-button>
-              </div> -->
-            </template>
-          </div>
+            </div>
+          </template>
+          <template v-else>
+            -
+          </template>
         </div>
-        <div v-else class="noInformation">{{ $t("__noInformation") }}</div>
       </div>
-    </div>
-    <div class="view-footer">
-      <el-pagination
-        layout="prev, pager, next, jumper, sizes"
-        :total="totalCount"
-        background
-        :page-size="pageSize"
-        :page-sizes="pageSizes"
-        :pager-count="pagerCount"
-        :current-page.sync="currentPage"
-        @size-change="handleSizeChangeByClient"
-        @current-change="handlePageChangeByClient"
-      />
+      <div v-else class="noInformation">{{ $t("__noInformation") }}</div>
     </div>
 
     <editDialog
       ref="createDialog"
-      :title="`${$t('__create')}${$t('__account')}`"
+      :title="`${$t('__create')}${$t('__agentOrSubAccount')}`"
       :visible="curDialogIndex === dialogEnum.create"
       :confirm="$t('__confirm')"
       :form="selectForm"
@@ -146,7 +99,7 @@
 
     <editDialog
       ref="editDialog"
-      :title="$stringFormat(`${$t('__edit')}${$t('__account')} - ID:{0}`, [selectForm.id])"
+      :title="$stringFormat(`${$t('__edit')}${$t('__agentOrSubAccount')} - ID:{0}`, [selectForm.id])"
       :visible="curDialogIndex === dialogEnum.edit"
       :confirm="$t('__revise')"
       :form="selectForm"
@@ -178,7 +131,9 @@ export default {
       }),
       searchForm: {},
       selectForm: {},
-      curDialogIndex: 0
+      curDialogIndex: 0,
+      searchH: 0,
+      topH: 0
     }
   },
   computed: {
@@ -191,10 +146,10 @@ export default {
   },
   methods: {
     remarkExpand(row) {
-      const obj = this.tableData.find(item => item.id === row.id);
+      const obj = this.allDataByClient.find(item => item.id === row.id);
       this.$nextTick(() => {
         obj.open = !obj.open;
-        this.tableData = JSON.parse(JSON.stringify(this.tableData))
+        this.allDataByClient = JSON.parse(JSON.stringify(this.allDataByClient))
       })
     },
     handleRespone(res) {
@@ -264,95 +219,171 @@ export default {
   }
 }
 </script>
-
 <style lang="scss" scoped>
-.view {
-  &-container {
-    &-table {
-      &-row {
-        position: relative;
+.tableStyle {
+  overflow: auto;
+  max-height: 100%;
+}
+.searchFrame {
+  padding-top: 1.5rem;
+  border-bottom: 0.25rem solid #f9c901;
+  flex-flow: wrap;
+  .pad_frame {
+    padding: 0 0.83333rem 0.41667rem 0.83333rem;
+  }
+  .searchItemBox {
+    width: 22.16667rem;
+    justify-content: space-between;
+  }
+  .searchIcon {
+    fill: #000 !important;
+    height: 1.5rem;
+    width: 1.5rem
+  }
+  .frame {
+    padding-bottom: 0;
+    border-bottom: 0;
+  }
+  .input_filter {
+    height: 2.66667rem;
+    line-height: 2.66667rem;
+    width: 100%;
+  }
+  .ctrlBtn {
+    justify-content: center;
+    align-items: center;
+    width: 3.1rem;
+    height: 2.8rem;
+  }
+  .searchBtn {
+    border-radius: 0.16667rem;
+    background-color: #f9c901;
+  }
+  .addBtn {
+    width: 4rem;
+    height: 2.8rem;
+  }
+  .searchItem {
+    width: 10.84583rem;
+    padding-right: 0;
+    padding-left: 0;
+  }
+  .searchIcon {
+    fill: #000 !important;
+  }
+  .wrap {
+    flex-flow: wrap;
+  }
+}
+
+.dataContent {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 0.625rem 1.16667rem 0.625rem 1.16667rem;
+  .base {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    .left {
+      width: 50%;
+      display: flex;
+      padding-top: 1rem;
+      padding-bottom: 1rem;
+      .spcolor {
+        color: #ce9600;
+      }
+      .accountnews {
         display: flex;
-        flex-direction: column;
-        .base {
-          width: 100%;
-          display: flex;
-          flex-direction: row;
-          .left {
-            width: 50%;
-            display: flex;
-            .spcolor {
-              color: #c49136;
-            }
-            .accountnews {
-              display: flex;
-              flex-direction: row;
-              .icon {
-                color: #c49136;
-              }
-            }
-          }
-          .right {
-            width: 50%;
-            display: flex;
-            flex-direction: column;
-          }
-          .setup {
-            display: flex;
-            flex-direction: row;
-            justify-content: flex-end;
-            margin-top: 10px;
-          }
+        flex-direction: row;
+        margin-top: 0.5rem;
+        .icon {
+          width: 1.5rem;
+          height: 1.5rem;
+          margin-right: 0.25rem;
+          color: #ce9600;
         }
-        .field {
-          display: flex;
-          flex-direction: column;
-          .title {
-            display: flex;
-            align-items: center;
-            width: 100px;
-            min-width: 100px;
-          }
-          .news {
-            font-weight: bold;
-            word-break: break-all;
-            color: #2b3c43;
-          }
-        }
-        .moreopen {
-          position: absolute;
-          top: 5px;
-          right: 5px;
-          font-size: 25px;
-          color: #4e4e4e;
-        }
-        .moreclose {
-          position: absolute;
-          bottom: 5px;
-          right: 5px;
-          font-size: 25px;
-          color: #4e4e4e;
+        .news {
+          margin-top: 0rem;
         }
       }
+    }
+    .right {
+      width: 50%;
+      display: flex;
+      flex-direction: column;
+      padding-top: 1rem;
+      padding-bottom: 1rem;
+    }
+    .setup {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
+      margin-top: 1rem;
+    }
+  }
+}
+.field {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  .title {
+    display: flex;
+    align-items: center;
+  }
+  .news {
+    font-weight: bold;
+    word-break: break-all;
+    margin-top: 0.5rem;
+    color: #2b3c43;
+  }
+}
+
+.expandInfo {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  display: flex;
+  .news {
+    margin-top: 0.5rem;
+  }
+  .left,
+  .right {
+    width: 50%;
+  }
+  .right {
+    display: flex;
+    .adminCtrl {
+      width: 50%;
     }
   }
 }
 
-@media screen and (min-width: 992px) {
-  .view {
-    &-container {
-      &-table {
-        &-row {
-          .item {
-            width: 450px;
-            min-width: 450px;
-            margin-right: 50px;
-          }
-          .operate {
-            width: 160px;
-          }
-        }
-      }
-    }
+.even-row {
+  background-color: #fff;
+}
+
+.odd-row {
+  background-color: #f4f4f4;
+}
+
+.moreopen {
+  position: absolute;
+  top: 0.83333rem;
+  right: 1.3rem;
+  .icon {
+    fill: #a3a3a3;
+    width: 2.5rem;
+    height: 2.5rem;
+  }
+}
+.moreclose {
+  position: absolute;
+  top: 8.33333rem;
+  right: 1.33rem;
+  .icon {
+    fill: #a3a3a3;
+    width: 2.333rem;
+    height: 2.333rem;
   }
 }
 </style>
