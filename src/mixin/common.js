fe7b1d07356256d968569e3793631280f141ad19
JoeTzeng <joe.tzeng@9talent.com>
2022-08-11 14:00:17 +0800

    上傳

--- src/mixin/common.js
+++ src/mixin/common.js
@@ -15,6 +15,42 @@ export default {
         callBack()
       }).catch(() => {
       })
+    },
+    addSelectDropDownFilter(name, onSelectAll, onClear, onInputFilter) {
+      const selectAll = document.createElement('span')
+      selectAll.className = 'text-link'
+      selectAll.onclick = onSelectAll
+      selectAll.innerHTML = `${this.$t('__selectAll')}`
+      const separator = document.createElement('span')
+      separator.className = 'pl-2 pr-2'
+      separator.innerHTML = '|'
+      const clear = document.createElement('span')
+      clear.className = 'text-link'
+      clear.onclick = onClear
+      clear.innerHTML = `${this.$t('__clear')}`
+      const group = document.createElement('div')
+      group.className = 'w-100 text-right'
+      group.appendChild(selectAll)
+      group.appendChild(separator)
+      group.appendChild(clear)
+      const innerBox = document.createElement('div')
+      innerBox.appendChild(group)
+      const selectFilter = document.createElement('div')
+      selectFilter.className = 'select-filter'
+      selectFilter.appendChild(innerBox)
+      const filterOption = document.createElement('div')
+      filterOption.className = 'el-filter_option'
+      filterOption.appendChild(selectFilter)
+      const inputFilter = document.createElement('input')
+      inputFilter.type = 'text'
+      inputFilter.autocomplete = 'off'
+      inputFilter.className = 'el-select__input el-filter_input w-100'
+      inputFilter.addEventListener("input", onInputFilter)
+      const filter = document.createElement('div')
+      filter.className = 'el-filter'
+      filter.appendChild(inputFilter)
+      filter.appendChild(filterOption)
+      document.getElementsByClassName(name)[0].getElementsByClassName('custom-dropdown')[0].prepend(filter)
     }
   }
 }
--- src/views/memberBet/index.vue
+++ src/views/memberBet/index.vue
@@ -287,56 +287,20 @@ export default {
   },
   methods: {
     addSelectFilter() {
-      this.onVisibleChange('options agents', () => {
+      this.addSelectDropDownFilter('options agents', () => {
         this.searchForm.agent_id = JSON.parse(JSON.stringify(this.searchItems.agents)).map(item => item.key)
       }, () => {
         this.searchForm.agent_id = []
       }, () => {
         this.selectOption.agents = JSON.parse(JSON.stringify(this.searchItems.agents)).filter(item => item.nickname.match(new RegExp(`${event.target.value}`, 'i')))
       })
-      this.onVisibleChange('options players', () => {
+      this.addSelectDropDownFilter('options players', () => {
         this.searchForm.member_id = JSON.parse(JSON.stringify(this.searchItems.members)).map(item => item.key)
       }, () => {
         this.searchForm.member_id = []
       }, () => {
         this.selectOption.members = JSON.parse(JSON.stringify(this.searchItems.members)).filter(item => item.nickname.match(new RegExp(`${event.target.value}`, 'i')))
       })
-    },
-    onVisibleChange(name, onSelectAll, onClear, onInputFilter) {
-      const selectAll = document.createElement('span')
-      selectAll.className = 'text-link'
-      selectAll.onclick = onSelectAll
-      selectAll.innerHTML = `${this.$t('__selectAll')}`
-      const separator = document.createElement('span')
-      separator.className = 'pl-2 pr-2'
-      separator.innerHTML = '|'
-      const clear = document.createElement('span')
-      clear.className = 'text-link'
-      clear.onclick = onClear
-      clear.innerHTML = `${this.$t('__clear')}`
-      const group = document.createElement('div')
-      group.className = 'w-100 text-right'
-      group.appendChild(selectAll)
-      group.appendChild(separator)
-      group.appendChild(clear)
-      const innerBox = document.createElement('div')
-      innerBox.appendChild(group)
-      const selectFilter = document.createElement('div')
-      selectFilter.className = 'select-filter'
-      selectFilter.appendChild(innerBox)
-      const filterOption = document.createElement('div')
-      filterOption.className = 'el-filter_option'
-      filterOption.appendChild(selectFilter)
-      const inputFilter = document.createElement('input')
-      inputFilter.type = 'text'
-      inputFilter.autocomplete = 'off'
-      inputFilter.className = 'el-select__input el-filter_input w-100'
-      inputFilter.addEventListener("input", onInputFilter)
-      const filter = document.createElement('div')
-      filter.className = 'el-filter'
-      filter.appendChild(inputFilter)
-      filter.appendChild(filterOption)
-      document.getElementsByClassName(name)[0].getElementsByClassName('custom-dropdown')[0].prepend(filter)
     },
     setSearchOpen() {
       this.searchOpen = !this.searchOpen
